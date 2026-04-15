c89d9fb9cbaae738fca157b45fc0131784e28c1e
avadhutkokane1612 <avadhutkokane1612@gmail.com>
2026-04-15 11:00:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function generateReport(){
+function markAttendance() {
+    let name = document.getElementById("name").value;
+    let status = document.getElementById("status").value;
 
-document.getElementById("result").innerHTML =
-"Total Present: 25 | Total Absent: 5";
+    if(name === "") {
+        alert("Enter name");
+        return;
+    }
 
+    let table = document.getElementById("table");
+
+    let row = table.insertRow();
+    row.insertCell(0).innerHTML = name;
+    row.insertCell(1).innerHTML = status;
+
+    // save data
+    localStorage.setItem("attendance", table.innerHTML);
+}
+
+window.onload = function () {
+    let data = localStorage.getItem("attendance");
+    if (data) {
+        document.getElementById("table").innerHTML = data;
+    }
 }
