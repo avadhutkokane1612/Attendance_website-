58b4352b137ab594bc6e7de1834f0e43aba87769
avadhutkokane1612 <avadhutkokane1612@gmail.com>
2026-04-15 11:46:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,21 @@ function markAttendance() {
     row.insertCell(1).innerHTML = status;
 
     // save data
-    localStorage.setItem("attendance", table.innerHTML);
+    function markAttendance() {
+  let name = document.getElementById("name").value;
+  let status = document.getElementById("status").value;
+  let table = document.getElementById("table");
+
+  let row = table.insertRow(-1);
+
+  row.insertCell(0).innerHTML = table.rows.length - 1;
+  row.insertCell(1).innerHTML = name;
+  row.insertCell(2).innerHTML = new Date().toISOString().split('T')[0];
+  row.insertCell(3).innerHTML = status;
+
+  // 🔥 important (overwrite)
+  localStorage.setItem("attendance", table.innerHTML);
+    }
 }
 console.log("JS WORKING");
 window.onload = function () {
